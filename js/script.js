6ee370dc0444a861c31fc9fe5d2652932d1ffb1b
Beqa Tsiramua <97254843+beqatsiramua1@users.noreply.github.com>
2022-08-05 11:43:55 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let myInfo = {
+  firstName: "Beqa",
+  lastName: "Tsirmaua",
+  age: 21,
+  city: "Tbilisi",
+  email: "beka.tsiramua.1@iliauni.edu.ge"
+}
+
+console.log(myInfo.firstName)
+console.log(myInfo.lastName)
+console.log(myInfo.age)
+console.log(myInfo.city)
+console.log(myInfo.email)
